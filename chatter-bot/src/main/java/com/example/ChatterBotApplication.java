f05bedca814f8bf871d3025d3ec72f1b2bb384cb
Kris De Volder <kdevolder@pivotal.io>
2016-07-13 15:32:20 -0700

    Playing around a bit trying to create some examples of configuring bot

--- chatter-bot/src/main/java/com/example/ChatterBotApplication.java
+++ chatter-bot/src/main/java/com/example/ChatterBotApplication.java
@@ -1,15 +1,19 @@
 package com.example;
 
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.integration.monitor.IntegrationMBeanExporter;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.PropertyPlaceholderHelper;
 
 /**
  * Chatter bot application. A stupid little 'bot' which connects to our 'chat' channel
@@ -19,30 +23,48 @@ import org.springframework.integration.support.MessageBuilder;
  */
 @SpringBootApplication
 @EnableBinding(Source.class)
+@EnableScheduling
 public class ChatterBotApplication implements CommandLineRunner {
+	
+	private static final Logger log = LoggerFactory.getLogger(ChatterBotApplication.class);
 	
 	@Autowired
 	Source channel;
 
 	@Autowired
-	private MyProperties prop;
+	TaskScheduler scheduler;
+	
+	@Autowired
+	private PingBotProperties config;
+	
+	private PropertyPlaceholderHelper formatter = new PropertyPlaceholderHelper("${", "}");
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ChatterBotApplication.class, args);
 	}
-	
+
+	int i;
+
 	@Override
 	public void run(String... arg0) throws Exception {
-		int i = 0;
-		while (true) {
-			Thread.sleep(prop.getInterval());
-			send("This is message #"+(++i));
-		}
+		log.info("Initializing bot: {}", config.getUser());
+
+		scheduler.scheduleAtFixedRate(() -> {
+			String formatted = formatter.replacePlaceholders(config.getMessage(), messageParameters());
+			log.debug("fomatted message = {}", formatted);
+			send(formatted);
+		}, config.getInterval());
+	}
+
+	private Properties messageParameters() {
+		Properties params = new Properties();
+		params.put("u", config.getUser());
+		params.put("c",  ++i+"");
+		return params;
 	}
 
 	private void send(String msg) {
 		channel.output().send(MessageBuilder.withPayload(msg).build());
 	}
-	
 	
 }
