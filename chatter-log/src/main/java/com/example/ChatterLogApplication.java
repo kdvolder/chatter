c6238911ae96d335cdece36006c02338ac088185
Kris De Volder <kdevolder@pivotal.io>
2016-07-13 10:58:02 -0700

    Remove unnessary workaround for already fixed bug

--- chatter-log/src/main/java/com/example/ChatterLogApplication.java
+++ chatter-log/src/main/java/com/example/ChatterLogApplication.java
@@ -1,21 +1,12 @@
 package com.example;
 
-import javax.management.MBeanServer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.integration.monitor.IntegrationMBeanExporter;
-import org.springframework.jmx.export.annotation.AnnotationMBeanExporter;
-import org.springframework.jmx.export.naming.ObjectNamingStrategy;
-import org.springframework.jmx.support.RegistrationPolicy;
 import org.springframework.messaging.Message;
 
 /**
@@ -36,25 +27,5 @@ public class ChatterLogApplication {
 	public void logChatMessage(Message<String> msg) {
 		log.info(msg.getPayload());
 	}
-	
-	@Bean
-    public IntegrationMBeanExporter integrationMbeanExporter(
-            @Value("spring.jmx.default-domain") String domain, 
-            MBeanServer mBeanServer) {
-        IntegrationMBeanExporter exporter = new IntegrationMBeanExporter();
-        exporter.setDefaultDomain(domain);
-        exporter.setServer(mBeanServer);
-        return exporter;
-	}
-	
-	@Bean
-	@Primary
-	public AnnotationMBeanExporter mbeanExporter(ObjectNamingStrategy namingStrategy, MBeanServer mBeanServer) {
-	        AnnotationMBeanExporter exporter = new AnnotationMBeanExporter();
-	        exporter.setRegistrationPolicy(RegistrationPolicy.FAIL_ON_EXISTING);
-	        exporter.setNamingStrategy(namingStrategy);
-	        exporter.setServer(mBeanServer);
-	        return exporter;
-	}	
-	
+		
 }
