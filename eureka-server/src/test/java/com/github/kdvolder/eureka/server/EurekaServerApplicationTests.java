32f9b34a2fb27ee8a0b57eb12bf0d3163be07fc5
Kris De Volder <kdevolder@pivotal.io>
2016-07-25 15:29:08 -0700

    More easy to deploy apps to CF
     1) Use random-route: true whre possible => no name clashes
    
    2) Add a eureka-server app, so the example is 'complete'.

---
+++ eureka-server/src/test/java/com/github/kdvolder/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.github.kdvolder.eureka.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EurekaServerApplication.class)
+@WebAppConfiguration
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
