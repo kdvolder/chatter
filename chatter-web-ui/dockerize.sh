537e4aff584de7ff6b257beff4b00391a685bdde
Kris De Volder <kdevolder@pivotal.io>
2018-05-16 19:22:01 -0700

    Make web-ui find rabbitmq via dns

--- chatter-web-ui/dockerize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-WORKDIR=$(pwd)
-./mvnw clean package
-cp target/*.jar docker/fatjar.jar
-cd docker
-docker build -t kdvolder/chatter-web-ui .
--- ci/destroy-pipeline.sh
+++ ci/destroy-pipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fly -t tools destroy-pipeline -p chatter-toronto-demo
+fly -t tools destroy-pipeline -p chatter-k8s
